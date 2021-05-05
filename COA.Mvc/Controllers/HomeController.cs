3d95adb7a388e19be0f592292d7044623b4cf566
Bruno Ferrari <bruno91f@gmail.com>
2021-05-05 18:06:50 -0300

    add simple create view

--- COA.Mvc/Controllers/HomeController.cs
+++ COA.Mvc/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace COA.Mvc.Controllers
@@ -22,15 +23,52 @@ namespace COA.Mvc.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var client = _httpFactory.CreateClient("COA-Api");
-            var response = await client.GetAsync("/api/users");
-            if (response.StatusCode != HttpStatusCode.OK)
+            try
             {
-                RedirectToAction("Error", "Home");
+                var client = _httpFactory.CreateClient("COA-Api");
+                var response = await client.GetAsync("/api/users");
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+                string content = await response.Content.ReadAsStringAsync();
+                var users = JsonConvert.DeserializeObject<List<UserViewModel>>(content);
+                return View(users);
             }
-            string content = await response.Content.ReadAsStringAsync();
-            var users = JsonConvert.DeserializeObject<List<UserViewModel>>(content);
-            return View(users);
+            catch
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newUser);
+            }
+            try
+            {
+                var client = _httpFactory.CreateClient("COA-Api");
+                var jsonUser = JsonConvert.SerializeObject(newUser);
+                var stringContent = new StringContent(jsonUser, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("/api/users", stringContent);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
